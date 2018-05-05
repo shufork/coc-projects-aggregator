d048f1d9292358fd4d1d567ab70fa26043bd7db3
cj <jclazz@outlook.com>
2018-05-05 15:58:34 +0800

    submodule

--- up-build-coc.sh
+++ up-build-coc.sh
@@ -1,5 +1,5 @@
 #/bin/sh
 echo "update src and build docker images for sc-coc ..."
 git pull
-git submoudle update --recursive --remote
+git submodule update --recursive --remote
 ./mvnw clean package -DskipTests -P 'docker-build' -U -f ./sc-coc/pom.xml
